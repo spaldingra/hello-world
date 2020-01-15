fbe13e27e877ff2ffa700d8080d9e788f6156395
spaldingra <59939578+spaldingra@users.noreply.github.com>
2020-01-15 17:24:29 -0500

    Added files for Lab 1

---
+++ Hello.java
@@ -0,0 +1,22 @@
+/***************************************
+ * Hello.java
+ ***************************************
+ * Reid Spalding
+ * 1/15/20
+ * CMSC 255 - 901
+ **************************************/
+
+ public class Hello {
+
+ 	public static void main(String[] args) {
+
+ 		System.out.println("Reid Spalding");
+ 		System.out.println("Hello.java");
+ 		System.out.println("Description: Install Java and run a Java File");
+ 		System.out.println("Version Date: 1/15/20");
+ 		System.out.println("CMSC 255 - 901");
+ 		System.out.println();
+
+ 	}
+ 	
+ }
